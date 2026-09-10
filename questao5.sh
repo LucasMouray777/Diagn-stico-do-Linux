f7f81ded61a3b23312b1e462902cdf220862857f
Lucas Mouray <117998778+LucasMouray777@users.noreply.github.com>
2026-09-10 11:12:29 -0300

    Delete questao5.sh

--- questao5.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Questão 5 - Exibir e alterar a prioridade (nice/pri) do processo bash
-# Substitua 21387 pelo PID atual do seu bash (verifique com: echo $$)
-
-ps -o pid,ni,pri,cmd -p 21387
-
-renice -n 5 -p 21387
-
-ps -o pid,ni,pri,cmd -p 21387
